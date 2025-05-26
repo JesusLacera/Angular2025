3a69fa61f406a15321512353fa2d70685be52c03
JesusLacera <lacerajesus@gmail.com>
2025-05-26 09:39:45 +0200

    26/05/25

--- cursoWorkspace/projects/16_buscador_rest/src/app/service/buscador.service.ts
+++ cursoWorkspace/projects/16_buscador_rest/src/app/service/buscador.service.ts
@@ -4,35 +4,31 @@ import { Item } from '../model/Item';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BuscadorService {
+  urlBase: string = 'http://localhost:3000/buscador/';
 
+  constructor(private http: HttpClient) {}
 
-  urlBase:string = 'http://localhost:8000/buscador';
-
-  constructor(private http:HttpClient) { }
-
-  buscar(tematica:string):Observable<Item[]>{
-     /*const url_busqueda = `${this.urlBase}/buscar?tematica=${tematica}`;
+  buscar(tematica: string): Observable<Item[]> {
+    /*const url_busqueda = `${this.urlBase}/buscar?tematica=${tematica}`;
      return this.http.get<Item[]>(url_busqueda);*/
-     const url_busqueda = `${this.urlBase}/buscar`;
-     return this.http.get<Item[]>(url_busqueda,{params:{"tematica":tematica}});
+    const url_busqueda = `${this.urlBase}/buscar`;
+    // return this.http.get<Item[]>(url_busqueda,{params:{"tematica":tematica}});
+    return this.http.get<Item[]>(`${url_busqueda}/${tematica}`);
   }
 
-  alta(item:Item):Observable<void>{
-    console.log(JSON.stringify(item))
+  alta(item: Item): Observable<void> {
+    console.log(JSON.stringify(item));
     const url_alta = `${this.urlBase}/alta`;
-    let head=new HttpHeaders();
-    head.set("Content-Type","application/json")
-    return this.http.post<void>(url_alta, item,{headers:head});
+    let head = new HttpHeaders();
+    head.set('Content-Type', 'application/json');
+    return this.http.post<void>(url_alta, item, { headers: head });
   }
 
-  eliminar(tematica:string):Observable<Item[]>{
-    const url_eliminar=`${this.urlBase}/eliminar?tematica=${tematica}`;
+  eliminar(tematica: string): Observable<Item[]> {
+    const url_eliminar = `${this.urlBase}/eliminar?tematica=${tematica}`;
     return this.http.delete<Item[]>(url_eliminar);
   }
-
-
-
 }
